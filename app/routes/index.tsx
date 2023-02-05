7fb0fff9d4b3c77502e2f2c8ea6b29f1be90a2a6
Kara Brightwell <kara@153.io>
2023-02-05 22:42:37 +0000

    try doing optimistic ui (not sure it worked)

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
+import type { Item } from "@prisma/client";
 import { ActionArgs, json } from "@remix-run/node";
-import { Form, useActionData, useLoaderData } from "@remix-run/react";
+import { Form, useActionData, useLoaderData, useNavigation } from "@remix-run/react";
 import { ItemCreateOneSchema } from "prisma/generated/schemas";
 
 const db = new PrismaClient()
@@ -10,6 +11,8 @@ export async function loader() {
   return json(items)
 }
 
+const validateItem = (formData: FormData) => ItemCreateOneSchema.parse({ data: Object.fromEntries(formData) })
+
 export async function action({ request }: ActionArgs) {
   const form = await request.formData()
   const data = ItemCreateOneSchema.parse({ data: Object.fromEntries(form) })
@@ -18,15 +21,19 @@ export async function action({ request }: ActionArgs) {
   return null
 }
 
+const ItemView = ({ name, description }: Omit<Item, 'id'>) =>  <li>{name} {description}</li>
+
 export default function Index() {
   const items = useLoaderData<typeof loader>()
+  const navigation = useNavigation()
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <ul>
         {items.map(
-          item => <li key={item.id}>{item.name} {item.description}</li>
+          item => <ItemView key={item.id} {...item} />
         )}
+        {navigation.formData && <ItemView {...validateItem(navigation.formData).data} />}
         <li>
           <Form method="post">
             <input name="name" placeholder="name" required />
