536cc0f26be49be7e0f68761afb41336976a22f9
Kara Brightwell <kara@153.io>
2023-02-13 21:26:38 +0000

    more recursive

--- app/routes/locations/$id.tsx
+++ app/routes/locations/$id.tsx
@@ -10,10 +10,21 @@ export async function loader({ params }: LoaderArgs) {
 	const where = coerce({ id: 'number' }, params)
 	const location = await db.location.findFirstOrThrow({
 		where,
-		include: {
-			parent: true
-		}
+
 	})
+
+	const ancestors = location.parentId ? await db.$queryRaw<Location[]>`
+		WITH RECURSIVE ancestors AS (
+			SELECT id, name, "parentId"
+			FROM "Location"
+			WHERE id = ${location.parentId}
+			UNION ALL
+			SELECT l.id, l.name, l."parentId"
+			FROM "Location" l
+			JOIN ancestors a ON a."parentId" = l.id
+		)
+		SELECT * FROM ancestors;
+	` : []
 
 	const descendents = await db.$queryRaw<Location[]>`
 		WITH RECURSIVE descendents AS (
@@ -33,16 +44,21 @@ export async function loader({ params }: LoaderArgs) {
 		text: url('location', location)
 	})).toString('base64')
 
-	return json({location, barcode, descendents})
+	return json({location, barcode, descendents, ancestors})
 }
 
 export default function LocationPage() {
-	const {location, barcode, descendents} = useLoaderData<typeof loader>()
+	const {location, barcode, descendents, ancestors} = useLoaderData<typeof loader>()
 
 	return <div>
-		{location.parent && <h2><Link to={url('location', location.parent)}>{location.parent.name}</Link></h2>}
+		{ancestors.length > 0 && <ul>
+			{ancestors.map(ancestor => <li key={ancestor.id}>
+				<Link to={url('location', ancestor)}>{ancestor.name}</Link>
+			</li>)}
+			</ul>
+		}
 		<h1>{location.name}</h1>
-		{descendents.length > 0 && <ul>{descendents.map(child => <li key={child.id}><Link to={url('location', child)}>{child.name}</Link></li>)}</ul>}
+		{descendents.length > 0 && <ul>{descendents.map(descendent => <li key={descendent.id}><Link to={url('location', descendent)}>{descendent.name}</Link></li>)}</ul>}
 
 		<img src={`data:image/png;base64,${barcode}`} alt="" />
 	</div>
