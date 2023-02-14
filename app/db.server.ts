090666b73ec93042846856072e0ae10be234a022
Kara Brightwell <kara@153.io>
2023-02-14 08:01:45 +0000

    initialise prisma client once

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,3 +1,13 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient()
+declare global {
+  var __db: PrismaClient | undefined;
+}
+
+if (!global.__db) {
+	global.__db = new PrismaClient({
+		log: ['query'],
+	})
+}
+
+export default global.__db
