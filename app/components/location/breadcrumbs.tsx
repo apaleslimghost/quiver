3dfdf3a1dd29eed2152fa260af8aee247eebf354
Kara Brightwell <kara@153.io>
2023-02-24 18:12:46 +0100

    always link to top level locations page

--- app/components/location/breadcrumbs.tsx
+++ app/components/location/breadcrumbs.tsx
@@ -4,9 +4,8 @@ import { FC, HTMLAttributes } from "react";
 import url from "~/lib/url";
 
 export const Breadcrumbs: FC<{ ancestors: Location[] } & HTMLAttributes<HTMLUListElement>> = ({ ancestors, ...props }) => {
-	if(ancestors.length === 0) return null
-
 	return <ul {...props}>
+		<li><Link to="/locations">Locations</Link></li>
 		{ancestors.map(ancestor => <li key={ancestor.id}>
 			<Link to={url('location', ancestor)}>{ancestor.name}</Link>
 		</li>)}
