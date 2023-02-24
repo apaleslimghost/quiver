0fcaf04d90fb8270428808c8670b36a760f35c04
Kara Brightwell <kara@153.io>
2023-02-24 18:30:57 +0100

    fix nested lis

--- app/components/item/link.tsx
+++ app/components/item/link.tsx
@@ -3,7 +3,5 @@ import { Link } from "@remix-run/react";
 import url from "~/lib/url";
 
 export const ItemLink = (item: Item) =>  (
-	<li>
-	  {!Number.isNaN(item.id) ? <Link to={url('item', item)}>{item.name}</Link> : item.name}
-	</li>
+	!Number.isNaN(item.id) ? <Link to={url('item', item)}>{item.name}</Link> : item.name
 )
