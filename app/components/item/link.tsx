d7a8bde5e51997bae871d4516d6921d79c4c656f
Kara Brightwell <kara@153.io>
2023-02-26 08:54:35 +0000

    always return an element from itemlink

--- app/components/item/link.tsx
+++ app/components/item/link.tsx
@@ -3,5 +3,5 @@ import { Link } from "@remix-run/react";
 import url from "~/lib/url";
 
 export const ItemLink = (item: Item) =>  (
-	!Number.isNaN(item.id) ? <Link to={url('item', item)}>{item.name}</Link> : item.name
+	!Number.isNaN(item.id) ? <Link to={url('item', item)}>{item.name}</Link> : <>{item.name}</>
 )
