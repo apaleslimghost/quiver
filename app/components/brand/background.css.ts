5851d56f51d1bb9a44730addc7b0142c1cf17926
Kara Brightwell <kara@153.io>
2023-04-19 17:59:18 +0100

    steel

--- app/components/brand/background.css.ts
+++ app/components/brand/background.css.ts
@@ -1,8 +1,9 @@
 import { style } from "@vanilla-extract/css";
+import * as colours from './colours.css'
 
 export const main = style({
 	imageRendering: 'crisp-edges',
-	backgroundColor: 'black',
+	backgroundColor: colours.vars.steel[0],
 	backgroundImage: `
 		repeating-linear-gradient(60deg, transparent, transparent 1px, white 1px, white 16px),
 		repeating-linear-gradient(-45deg, transparent, transparent 1px, white 1px, white 16px)
