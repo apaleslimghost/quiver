8dcecd7b7319f088a6dab6b3a5a765236844b650
Kara Brightwell <kara@153.io>
2023-07-27 20:18:26 +0100

    subcategory creation form

--- app/routes/categories/$id.tsx
+++ app/routes/categories/$id.tsx
@@ -1,5 +1,5 @@
 import { LoaderArgs, json } from '@remix-run/node'
-import { Link, useLoaderData } from '@remix-run/react'
+import { Link, useLoaderData, Form as RemixForm } from '@remix-run/react'
 import { z } from 'zod'
 import { Heading } from '~/components/typography/heading'
 import db from '~/lib/db.server'
@@ -10,6 +10,8 @@ import * as queries from '~/lib/queries'
 import { Breadcrumbs } from '~/components/breadcrumbs'
 import grid from '~/components/layout/grid.css'
 import { ItemCard } from '~/components/item/card'
+import { Button, Form, Input } from '~/components/form/form'
+import { unstyled } from '~/components/typography/link.css'
 
 const CategoryParamsSchema = z.object({
 	id: z.coerce.number(),
@@ -28,11 +30,17 @@ export async function loader({ params }: LoaderArgs) {
 		? await queries.ancestors('Category', category.parentId)
 		: []
 
-	return json({ category, ancestors })
+	const children = await db.category.findMany({
+		where: {
+			parentId: where.id,
+		},
+	})
+
+	return json({ category, ancestors, children })
 }
 
 export default function CategoryPage() {
-	const { category, ancestors } = useLoaderData<typeof loader>()
+	const { category, ancestors, children } = useLoaderData<typeof loader>()
 
 	return (
 		<div className={container.area.content}>
@@ -43,6 +51,24 @@ export default function CategoryPage() {
 				{category.items.map((item) => (
 					<ItemCard key={item.id} item={item} />
 				))}
+
+				{children.map((child) => (
+					<Link key={child.id} to={url('category', child)} className={unstyled}>
+						<div className={card.card}>
+							<Heading level={3} className={card.title}>
+								{child.name}
+							</Heading>
+						</div>
+					</Link>
+				))}
+
+				<div className={card.card}>
+					<Form as={RemixForm} method='post' action={url('category', 'new')}>
+						<Input name='name' placeholder='name' required />
+						<input name='parentId' type='hidden' value={category.id} />
+						<Button type='submit'>Add subcategory</Button>
+					</Form>
+				</div>
 			</div>
 		</div>
 	)
